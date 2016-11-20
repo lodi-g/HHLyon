43e79684a439253c7185e76784507a9f88c93280
Maxxil <massil.kadi@ynov.com>
2016-11-20 09:52:08 +0100

    Modification route

--- route.js
+++ route.js
@@ -3,14 +3,14 @@
  */
 app.config(function($routeProvider){
     $routeProvider
-        .when("/",{templateUrl: "Connexion/index.php" })
+        .when("/",{templateUrl: "Inscription/Inscription.html" })
         .when("/accueil", {templateUrl: "Accueil/accueil.html" })
         .when("/symptomes",{templateUrl: "Accueil/symptomes.html" })
         .when("/questionnaire" , {templateUrl: "QuestionnaireMedical/QuestionnaireMedical.html"})
         .when("/verificationQuestionnaire/:id" , {templateUrl : "QuestionnaireMedical/GestionQuestionnaire.php"})
         .when("/inscription",{templateUrl: "Inscription/Inscription.html"})
         .when("/verificationInscription",{templateUrl: "Inscription/gestionInscription.html"})
-        .when("/connect" , {templateUrl : "Connexion/login.php"})
+        .when("/connect" , {templateUrl : "Connexion/index.php"})
         .when("/error", {templateUrl : "Connexion/error.html"})
         .otherwise({redirectTo : "/"});
 });
